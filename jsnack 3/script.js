5e20f917dd07f1246bf2363594e9f583a2a801fb
Eligio Biasi <ex_98@live.it>
2023-05-18 16:18:52 +0200

    start third snack

---
+++ jsnack 3/script.js
@@ -0,0 +1,10 @@
+// Snack 3:
+// Crea un array vuoto.
+const numberContainer = []
+// Chiedi per 6 volte all’utente di inserire un numero
+for(i=0;i<6;i++){
+    let insertNumber = parseInt(prompt('inserisci un numero'));
+    
+}
+
+// se è dispari inseriscilo nell’array.
