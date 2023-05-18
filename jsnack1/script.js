af81c8c2fc222aaa30a7581e6ae7739077a83749
Eligio Biasi <ex_98@live.it>
2023-05-18 15:42:50 +0200

    snack 1 completed

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,14 @@
 // Snack 1:
-// Il software deve chiedere per 10 volte all’utente di inserire un numero.
-// Il programma stampa la somma di tutti i numeri inseriti.
+// Il software deve chiedere per 10 volte all’utente di inserire un numero
+let sum = 0;
+for(i=0;i<10;i++){
+    const question = parseInt(prompt('inserisci un numero'));
+
+    // Il programma stampa la somma di tutti i numeri inseriti.
+    sum+=question
+    console.log(sum)
+}
+    
+
+
+
